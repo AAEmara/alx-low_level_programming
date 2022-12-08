c8b397df432336f80f7cbee90d927592d121f16c
emara <abdelrahmanemara18@gmail.com>
2022-12-08 15:11:24 -0800

    Adding 6th taks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main- Entrance of the program
+ *
+ *
+ *return- returns 0 (sucesses)
+ * **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
