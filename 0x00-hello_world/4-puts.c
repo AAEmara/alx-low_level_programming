db017006393be13567883e7835f1a77367e61462
emara <abdelrahmanemara18@gmail.com>
2022-12-08 14:57:38 -0800

    Adding 5th task's solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,11 @@
  *
  * Return: return 0 (success)
  * **/
-
 int main(void){
+
 	char statement[52];
 	
-	strcpy( statement, "\"Programming is like building a multilingual puzzle");
+	strcpy(statement, "\"Programming is like building a multilingual puzzle");
 	puts(statement);
-	return(0);
+	return (0);
 }
