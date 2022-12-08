c59ef1ab1ea0bf049a5c8833af58a702e693029a
emara <abdelrahmanemara18@gmail.com>
2022-12-08 14:51:04 -0800

    Adding 5th task's solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(void){
-	char statement;
+	char statement[52];
 	strcpy(statement, "\"Programming is like building a multilingual puzzle");
 	puts(statement);
 	return(0);
