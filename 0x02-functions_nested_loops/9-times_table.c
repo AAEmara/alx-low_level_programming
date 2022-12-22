174e8b52d3beb3eb4674799c1fc64bd718d62d80
emara <abdelrahmanemara18@gmail.com>
2022-12-21 20:25:07 -0800

    Fixing issue

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,20 @@ void times_table(void)
 	for (digit = 0; digit <= 9; digit++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			result = digit * mult;
+
 				if (result <= 9)
 					_putchar(' ');
 				else
+				{
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
+				}
 		}
 	_putchar('\n');
 	}
