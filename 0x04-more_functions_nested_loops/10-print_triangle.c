c13016f5b9706df7b5ac7b404e681e88d3392765
emara <abdelrahmanemara18@gmail.com>
2022-12-22 03:03:27 -0800

    Task 11

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,10 +13,12 @@ void print_triangle(int size)
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
 				_putchar(' ');
 
 				for (inc2 = 0; inc2 < inc1; inc2++)
 					_putchar('#');
+			}
 
 			if (inc1 == size)
 				continue;
