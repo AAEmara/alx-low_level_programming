5a1e02b26a9ca8fe5b4f0cd5a0bbd22ff8899954
emara <abdelrahmanemara18@gmail.com>
2022-12-24 06:59:22 -0800

    My version of solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <string.h>
+
 /**
- *main-Entrance of the program
- *
- *
- * Return: return 0 (success)
- * **/
+*main - Entry Point
+*
+*Description - Prints a statment followed by a newline
+*
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char statement[52];
-
-	strcpy(statement, "\"Programming is like building a multilingual puzzle");
-	puts(statement);
+	printf("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
