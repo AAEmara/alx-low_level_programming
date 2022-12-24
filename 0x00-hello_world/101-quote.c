088813f4c26dd4b387339ae3b43ac5e0b77fc7b3
emara <abdelrahmanemara18@gmail.com>
2022-12-24 10:55:44 -0800

    Added: task 8 without using printf or puts man 3 section

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+*main - Entry Point
+*
+*Description: Prints out a set of characters using putchar
+*
+*Return: Always (1) (stdrr)
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+}
