0339c9bc56d694afac2f22e0f296e781ce875228
emara <abdelrahmanemara18@gmail.com>
2023-01-02 15:28:46 -0800

    Added: Task 1 Source Code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,44 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
-*print_alphabet - function that print alphabet
-*it prints in lowercase
-*Return: no return
+* print_alphabet - A function that prints alphabets in lowercase format.
+*
+* @parameters: No parameters are used.
+*
+* Description: Prints the full sequence of alphabets in their original order
+* whenever the print_alphabet() function is invoked.
+*
+* Return: No Return.
 */
 void print_alphabet(void)
 {
-	int letter;
+	int i;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	i = 96;
+
+	while (i < 122)
 	{
-		_putchar (letter);
+		i++;
+		putchar(i);
 	}
 
-	_putchar (10);
+	putchar('\n');
+	return;
+}
+
+/**
+* main - Entry point of the program.
+*
+* @parameter: No parameters are used.
+*
+* Description: Prints the alphabets in lowercase in ascending order
+* by calling the print_alphabet() function.
+*
+* Return: Always 0 (Success).
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
