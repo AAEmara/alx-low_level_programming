7c0e59439da453c1c9cfba83e318b9321dbb5227
emara <abdelrahmanemara18@gmail.com>
2023-01-02 14:59:17 -0800

    Modified: Removed the function and used printf only

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 
-int _putchar();
-
-/**
-* _putchar - A function that outputs "_putchar" followed by newline.
-*
-* @parameter: No parameters used.
-*
-* Description: This function is used to print the statement "_putchar"
-* followed by a newline "\n" whenever the function is invoked.
-*
-* Return: Always 0 (Success).
-*/
-int _putchar()
-{
-	printf("_putchar\n");
-	return (0);
-}
-
 /**
 * main - Entry point of the program.
 *
-* @parameter: No parameters used.
-*
-* Description: This function calls the _putchar function.
+* Description: Prints "_printchar" followed by a newline "\n".
 *
 * Return: Always 0 (Success).
 */
 int main(void)
 {
-	_putchar();
+	printf("_putchar\n");
 	return (0);
 }
