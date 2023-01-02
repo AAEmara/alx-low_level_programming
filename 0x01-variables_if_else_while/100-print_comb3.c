b660ae8622131c34828d7bb2e0dc61c6784ef5b0
emara <abdelrahmanemara18@gmail.com>
2023-01-01 18:16:31 -0800

    Added: Advanced Task 1 Source Code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program.
+*
+* Description: Prints the unique combination of two different numbers.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	int j;
+
+	i = 48;
+	j = 49;
+
+	while (i < 57)
+	{
+		putchar(i);
+		putchar(j);
+		j++;
+
+		if (i != 56)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+
+		if (j > 57)
+		{
+			i++;
+			j = i+ 1;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
