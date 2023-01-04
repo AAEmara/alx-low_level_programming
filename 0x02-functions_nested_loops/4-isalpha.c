e89768cd09d589307e37b1c591c0b51f3cdcb4a5
emara <abdelrahmanemara18@gmail.com>
2023-01-04 12:14:31 -0800

    Added: Task 4 Source Code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,18 @@
 #include "main.h"
+
 /**
-*_isalpha - controls if a character is alphabetical
-*@c: character to be verified
-*Return: return 0 or 1
+* _isalpha - Checks if the char is a lowercase or uppercase letter.
+*
+* Retrun: 1 (Success) or 0 (Fail)
 */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ( c >= 'a' && c <= 'z')
 		return (1);
-	return (0);
+		
+	else if ( c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
