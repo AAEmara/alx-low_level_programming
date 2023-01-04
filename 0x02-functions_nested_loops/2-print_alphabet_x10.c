0cdb7e7d5a473e99ac358138ce1ebf2bc6d75e7c
emara <abdelrahmanemara18@gmail.com>
2023-01-04 05:46:05 -0800

    Added: Task 2 Source Code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,20 @@
 #include "main.h"
+
 /**
-*print_alphabet_x10 - prints alphabet letters
-*ten times
-*desc: The function uses loop to operate
-*Return: no return
+* print_alphabet_x10 - Prints the alphabet in lowercase followed by a newline
+* 10 times.
+*
+* Return: No return.
 */
 void print_alphabet_x10(void)
 {
-	int counter = 0;
+	int i;
 
-	int letter;
+	i = 0;
 
-	while (counter++ <= 9)
+	while (i < 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-		_putchar(10);
+		print_alphabet();
+		i++;
 	}
 }
