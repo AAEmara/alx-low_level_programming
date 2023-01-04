9689fc1d7bc45c17a4661a1426ea072b254122dd
emara <abdelrahmanemara18@gmail.com>
2023-01-04 12:28:29 -0800

    Modified: removed trailing spaces after parenthesis and added param description - Betty Code Style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,15 @@
 /**
 * _isalpha - Checks if the char is a lowercase or uppercase letter.
 *
-* Retrun: 1 (Success) or 0 (Fail)
+* @c: Character type parameter to be checked by _isalpha function.
+* Retrun: returns 0 or 1.
 */
 int _isalpha(int c)
 {
-	if ( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
-		
-	else if ( c >= 'A' && c <= 'Z')
+
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 
 	else
