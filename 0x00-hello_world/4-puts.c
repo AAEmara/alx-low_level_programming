6c1d053a9ae8288ddec557d647c1c8ee0c9e19c6
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-18 17:00:05 -0700

    Task4 C source code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle to the
+ * standard output.
+ *
+ * Retrun: Always 0 (Success)
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
