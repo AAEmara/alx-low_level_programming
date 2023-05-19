bd9d2f461000c940179efba26101ff9c3529dbe8
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-18 17:08:42 -0700

    Task5 C source code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
