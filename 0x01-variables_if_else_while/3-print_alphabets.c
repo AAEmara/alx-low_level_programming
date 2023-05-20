729cced2a5623eaa2f5f990a6efe613e3b01230d
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-19 18:41:00 -0700

    Task3 C source code and removed Task0 compiled file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		putchar(start);
+		start++;
+	}
+
+	start = 'A';
+
+	while (start <= 'Z')
+	{
+		putchar(start);
+		start++;
+	}
+	putchar('\n');
+
+	return (0);
+}
