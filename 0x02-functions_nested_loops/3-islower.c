63b0f32dd2d2334b47217ef727504b21a2dd7f37
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-23 19:03:44 -0700

    Task3 C source code and update Header file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if the input character is lowercase or not.
+ *
+ * @c: Integer value of the character used to be checked in function.
+ *
+ * Return: 0 on Success.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
