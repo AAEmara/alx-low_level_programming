067e431f34c777028c3aba08a806580386b2a85c
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-27 19:45:00 -0700

    Task0 C source code, Header and README file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: Character value for the input to be checked by the function.
+ *
+ * Return: 1 if uppercase
+ *	   0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
