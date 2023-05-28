a74ba09f8c2ec47d816247c2a9becf6d7f584170
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-05-27 20:06:06 -0700

    Task2 C source code and Header file updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integer by each other.
+ * @a: Integer value to be multiplied by the second integer.
+ * @b: Integer value to be multiplied by the first integer.
+ *
+ * Return: Product of the two integers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
