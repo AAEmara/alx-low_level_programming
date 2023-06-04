0ca4346a2f62ab82c32cf229ee8f7059ae6eb882
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-03 22:06:33 -0700

    Task0 C source code, Header file and README file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value the pointer  points to, to 98.
+ * @*n: Pointer to an Integer value parameter.
+ *
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
