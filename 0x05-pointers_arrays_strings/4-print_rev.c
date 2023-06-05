ad1f312abde22007b876f22a3de2458c9a28e3b9
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-04 23:54:35 -0700

    Task4 C source code 1st Fix
    Added _strlen function in the source code since
    ALX's checker won't have the 2-strlen.c in its
    compilation process.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,3 +17,22 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer that points to a Character value.
+ *
+ * Return: String Length (Always +ve or Zero).
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
