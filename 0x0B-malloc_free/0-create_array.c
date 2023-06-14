dff83ea33f56fc5512dedfc7b0eaee409972a5be
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-13 18:02:42 -0700

    Task0 C source code, Header file and README file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars, and initialize it with
+ * a specific character.
+ * @size: Unsigned Integer value parameter indicating the size of the array.
+ * @c: Character value parameter that the array will be initialized with it.
+ *
+ * Return: NULL if size is 0 or it fails,
+ *	   Address of the Pointer.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr = (char *)malloc(sizeof(c) * size);
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			return (arr);
+
+		}
+
+	}
+}
