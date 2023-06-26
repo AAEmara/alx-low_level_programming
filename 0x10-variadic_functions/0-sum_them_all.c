de8e0035691b8acf47375b63160dba1f679ba7b0
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-26 07:55:35 -0700

    0x10: Task0 C source code, Header file and README

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns the sum of all of its parameters.
+ *
+ * @n: Number of parameters to be summed.
+ *
+ * Return: 0 if n == 0
+ *	   else returns Integer Value
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_list args;
+
+		int i, x;
+		int sum = 0;
+
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			x = va_arg(args, int);
+			sum += x;
+		}
+		va_end(args);
+
+		return (sum);
+	}
+}
