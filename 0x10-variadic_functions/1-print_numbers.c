303d23019eadf27abd1a7b2240c54a25f9c9bacb
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-26 08:36:35 -0700

    0x10: Task1 C source code and Updated Header file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers, followed by a newline.
+ *
+ * @separator: Character value that is used to separate between numbers.
+ * @n: Unsigned Integer value that represents the number of integers to be
+ * printed.
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int x;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		if (n - i == 1)
+		{
+			printf("%d\n", x);
+		}
+		else
+		{
+			printf("%d%s", x, separator);
+		}
+	}
+	va_end(args);
+}
