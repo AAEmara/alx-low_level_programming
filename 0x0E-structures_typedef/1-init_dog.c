ac96c228e9d8cacd953f59bcbd47164b08de7b38
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-29 04:35:00 -0700

    0x0E: Task1 C source code 1st Fix
    Handling the Pointer to Structure value if
    it was given a NULL value.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Initializes a dog structure.
@@ -12,7 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+	{
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
