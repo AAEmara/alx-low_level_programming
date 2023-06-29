cc87d580be7157d43f9ebc7fa4ae7226f335f95e
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-29 11:20:53 -0700

    0x06: Task4 C source code and Updated Header file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ *
+ * @a: Pointer to Integer value, where its array will be reversed.
+ * @n: Integer value for the number of elements inside the array.
+ *
+ * Return: Nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i <= (n / 2); i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
