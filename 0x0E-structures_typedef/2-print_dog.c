1dbe18fe69ef0c310813ff236b0d0779a60943b6
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-29 05:07:07 -0700

    0x0E: Task2 C source code and Updated Header file
    Made a function that prints the content of a dog
    Structure.
    
    Updated the Header file with the prototype of the
    function used.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+
+/**
+ * print_dog - Prints the elements of a dog Structure.
+ *
+ * @d: Pointer to a dog Structure, where it takes the address of the
+ * dog Structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		/**
+		 * Handling the pointer pointing to NULL by printing nothing
+		 */
+	}
+	else
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == 0)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
