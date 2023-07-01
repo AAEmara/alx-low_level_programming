062f0970372e221cb27f6dbb3b2ca3608b6b1621
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-30 19:59:56 -0700

    Task0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,8 +15,10 @@ size_t print_list(const list_t *h)
 
 	while (status)
 	{
+		if (h == NULL)
+			return (NULL);
 
-		if (h->next == NULL)
+		else if (h->next == NULL)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 			sum++;
