6b2e148a7ef97b13d217054919427a0a5309f2f4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-06-30 19:58:10 -0700

    0x12: Task0 C source code, Header and README file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of the given list.
+ *
+ * @h: Pointer to list_t defined data type where its elements are going
+ * to be printed.
+ *
+ * Return: Number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int status = 1;
+	int sum = 0;
+
+	while (status)
+	{
+
+		if (h->next == NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			sum++;
+			status = 0;
+		}
+		else
+		{
+			if (h->str == NULL)
+				printf("[%d] %s\n", 0, "(nil)");
+			else
+				printf("[%d] %s\n", h->len, h->str);
+			sum++;
+			h = h->next;
+		}
+	}
+	return (sum);
+}
