94b1b2b8370d3e9b4e55f376f19fdfc25935f9b8
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-02 19:00:45 -0700

    0x08: Task0 C source code, Header and README file
    Added a recursive function that prints a string
    without the use of any for loops or static
    variables.
    
    Added the functions prototypes used in the Header
    file.
    
    Added the README file.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line in a recursive
+ * way.
+ *
+ * @s: Pointer to Character value, which is the string to be printed.
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*(s));
+		_puts_recursion(s + 1);
+	}
+}
