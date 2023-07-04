2d642ff2a9183abc223eb5b7fd73073727b876b4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-04 01:57:20 -0700

    0x0B: Task3 C source code and Updated Header file
    Added a new function that allocates a memory in
    the heap during runtime (dynamic allocation) for
    a 2-dimensional array, and initializes its
    elements with 0 value.
    
    Updated the Header file with the function's
    prototype.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Returns a pointer to a 2 dimensional array of integers
+ * that are initialized by zeros.
+ *
+ * @width: Integer value, which is the number of columns of the 2 dimensional
+ * array.
+ * @height: Integer value, which is the number of rows of the 2 dimensional
+ * array.
+ *
+ * Return: NULL on failure of dynamic allocation
+ *	   NULL if width or height are negative or zero
+ *	   Address of the Pointer to the 2 dimensional array.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid = (int **)malloc(sizeof(int *) * (height));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	else if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < height; i++)
+		{
+			grid[i] = (int *)malloc(sizeof(int) * width);
+		}
+
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+		return (grid);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *str);
 char *str_concat(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
+int **alloc_grid(int width, int height);
+
+#endif
