7fe659ed5a7a28757aa655677e9816a59620e82d
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-04 19:56:55 -0700

    Task4 C source code 5th Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,11 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog2 == NULL)
 	{
-		free(dog2->name);
-		free(dog2->owner);
 		free(dog2);
-		free(name);
-		free(owner);
 		return (NULL);
 	}
 	else
