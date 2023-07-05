185d7b503223205a8af62015dff73185cbeb4c34
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-04 18:26:19 -0700

    0x0C: Task1 C source code 3rd Fix
    Handeled the NULL input for both strings
    correctly.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,21 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j = strlen(s1);
+	unsigned int i, j;
 	char *str;
+
+	if (s1 == NULL)
+		s1 = "\0";
+
+	if (s2 == NULL)
+		s2 = "\0";
 
 	if (n > strlen(s2))
 		n = strlen(s2);
 	else if (n < strlen(""))
 		n = 0;
+
+	j = strlen(s1);
 
 	str = malloc(sizeof(*s1) * (strlen(s1) + n) + 1);
 	if (str == NULL)
@@ -31,16 +39,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		free(str);
 		return (NULL);
 	}
-	if (s1 == NULL)
-		s1[0] = '\0';
-
-	if (s2 == NULL)
-		s2[0] = '\0';
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		str[i] = s1[i];
-	}
 
 	for (i = 0; s2[i] != '\0' && i < n; i++)
 	{
