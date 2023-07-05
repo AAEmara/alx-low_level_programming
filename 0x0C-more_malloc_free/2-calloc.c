270d565ff9cb89715919ebe182ed726e6b2f8c9f
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-05 14:18:12 -0700

    0x0C: Task2 C source code and Updated Header file
    Added a function that mimics the work of calloc()
    function.
    
    Updated the Header file with the funciton's
    prototype.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array using malloc function.
+ *
+ * @nmemb: Unsigned integer value, which is the number of elements
+ * in the array to be allocated.
+ * @size: Unsigned integer value, which is the size to be allocated
+ * for each single element of the array.
+ *
+ * Description: The function initializes the memory allocated with
+ * zero values.
+ *
+ * Return: Address of the pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	ptr = malloc(98 * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < nmemb; i++)
+			ptr[i] = 0;
+		return (ptr);
+	}
+}
