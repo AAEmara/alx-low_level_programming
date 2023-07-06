26c05886cab8930341210e42ec12f322a279b372
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-05 17:46:49 -0700

    0x07: Task1 C source code and Updated Header file
    Added a function that copies `n` bytes of a string
    to another string.
    
    Updated the Header file with the function's
    prototype.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area.
+ *
+ * @dest: Pointer to character value, which is the area in memory where the
+ * original string is copied to.
+ * @src: Pointer to character value, which is the original string to be
+ * copied.
+ * @n: Unsigned integer value, which is the number of bytes to be copied.
+ *
+ * Return: Address of the string.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; src[i] != '\0' || i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
