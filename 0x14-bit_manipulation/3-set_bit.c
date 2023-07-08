76f2ae0aac339a8e7314e896f0ea0a6bda48efce
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-08 03:52:50 -0700

    0x14: Task3 C source code and Updated Header file
    Added a function that changes the required bit
    of a number according to a given index into 1
    and sets the new number to a new one according to
    the changed bit.
    
    Updated the Header file with the funciton's
    prototype.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 to a given index.
+ *
+ * @n: Pointer to Unsigned Long Integer Value to be changed.
+ * @index: Unsigned Integer value, represents the index of the bit that
+ * will be changed (starting from index 0).
+ *
+ * Returns: -1 if Failed.
+ *	    1 if Succeeded.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	else
+	{
+		*n |= 1 << index;
+		return (1);
+	}
+}
