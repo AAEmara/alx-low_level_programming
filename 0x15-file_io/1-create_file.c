524a674ab128ddb3f82e7c497c2e5c998bb93986
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-11 16:17:57 -0700

    0x15: Task1 C source code 1st Fix
    Complied with Betty Coding Style only.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,16 +20,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT|O_EXCL, S_IRUSR|S_IWUSR);
+	fd = open(filename, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 	printf("Before fd < 0, fd = %d\n", fd);
 	if (fd < 0)
 	{
 		if (fd == EEXIST)
-			fd = creat(filename, O_CREAT|O_RDWR|O_TRUNC);
+			fd = creat(filename, O_CREAT | O_RDWR | O_TRUNC);
 	}
 	else
 	{
-		fd = creat(filename, S_IRUSR|S_IWUSR);
+		fd = creat(filename, S_IRUSR | S_IWUSR);
 	}
 
 	if (text_content == NULL)
