4e4b97ccb5d6db0fb934879a12b6e79fd04e103c
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-12 19:25:35 -0700

    0x15: Task1 C source code 4th Fix
    Fixed Truncating the file if it exists.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		fd = open(filename, O_CREAT | O_EXCL | O_RDWR | O_TRUNC,
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC,
 			  S_IRUSR | S_IWUSR);
 
 		if (fd < 0)
