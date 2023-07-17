6f198826739fc46f784b116b59e87e77b4dc4dfb
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-17 00:23:02 -0700

    0x07: Task2 C source code and Updated Header file
    Added a function that returns the address of a
    pointer to a character value when it the
    character matches the character in the string.
    
    Updated the Header file with the function's
    prototype.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr: Locates a character in a string.
+ *
+ * @s: Pointer to Character value, a string that will be compared to the given
+ * character.
+ * @c: Character value, that will be used in comparison with the string.
+ *
+ * Return: Address of the character in the string that matches.
+ *	   NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != c; i++)
+		s++;
+
+	if (s[i] == '\0')
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (s + 1);
+	}
+}
