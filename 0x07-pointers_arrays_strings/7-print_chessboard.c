fed1c9331d19d03d6209aaf6350b3cbf6f343599
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-29 09:52:09 -0700

    0x07: Task6 C source code 2nd Fix.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,10 +15,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (a[i][j] == ' ')
-				continue;
-
-			else if (j == 0 && i != 1)
+			if (j == 0 && i != 1)
 				_putchar(a[i][j]);
 
 			else
