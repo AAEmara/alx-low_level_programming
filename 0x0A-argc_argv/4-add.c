cecdb56fe539fc698b67cb1bf7f8465ed77e87fb
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-29 02:52:38 -0700

    0x0A: Task4 C source code Fix
    Handeled characters that are outside the range
    of '0' and '9'.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i;
+	int i, j;
 
 	if (argc == 1)
 	{
@@ -26,6 +26,19 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i <= argc - 1; i++)
 		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+					j++;
+
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		
 			if (*argv[i] >= '0' && *argv[i] <= '9')
 			{
 				sum += atoi(argv[i]);
