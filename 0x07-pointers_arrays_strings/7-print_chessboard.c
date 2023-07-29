3e73bfb02adf73f0d74046caae60a8b2a8ac790a
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-07-29 09:47:08 -0700

    0x07: Task6 C source code and Updated Header file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard.
+ *
+ * @a: Pointer to an array of 8 character.
+ *
+ * Return: Nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (a[i][j] == ' ')
+				break;
+
+			else if (j == 0 && i != 1)
+				_putchar(a[i][j]);
+
+			else
+				_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
