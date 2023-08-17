5d21434b3f4dd4cc162303364031cf91a98917d4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-17 10:28:35 -0700

    0x17: Task4 C source code, and Update Header File

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint  - Frees a `dlistint_t` list.
+ *
+ * @head: Pointer to a `dlistint_t` list type, which represents the
+ * 
+ * Return: Nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+}
