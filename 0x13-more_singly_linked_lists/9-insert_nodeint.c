c6bbf70d58a724b3415507664dc6eae8eb96cf52
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-18 09:29:16 -0700

    0x13: Task9 C source code 3rd Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,11 +22,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 
-	if (idx == 0)
+	if (idx == 0 && *head == NULL)
 	{
-		new_node->next = *head;
-		return (new_node);
+		*head = new_node;
+		return (*head);
 	}
 
 	while (count != idx - 1 && curr != NULL)
