5d9c2248426ab9f7b230807536f918f37f473542
AAEmara <abdelrahmanemara18@gmail.com>
2025-09-28 14:26:54 +0300

    feat(c): add program to utilize unix system calls
    Added a C-File, that includes a C program that utilizes
    the `write` system call to print to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,19 @@
 #include <unistd.h>
 
-/*
+/**
+ * main - Entry point
  *
+ * Description: Uses unix's library to write to the stdout a certain string.
+ *
+ * Return: 1 (STDOUT)
  */
-int main() {
-	ssize_t write(int 1; char* "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; int 1024);
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" ";
+	char str1[] = "- Dora Korpar, 2015-10-19\n";
+
+	write(2, str, sizeof(str) - 1);
+	write(2, str1, sizeof(str1) - 1);
+
 	return (1);
 }
