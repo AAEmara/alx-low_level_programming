02d4cba434af397c5e5553bde98825052e2ed56f
AAEmara <abdelrahmanemara18@gmail.com>
2025-09-28 12:49:30 +0300

    feat(c): add program to print output using printf
    Added a C-File, that includes a program that prints
    a formatted string using `printf` function.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line to the standard output.
+ * main - Entry point
+ *
+ * Description: Formats an output with printf function with a trailing newline.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
