957531b9afc0295faae0237b7bf7f560f42e6e68
AAEmara <abdelrahmanemara18@gmail.com>
2025-09-28 15:24:58 +0300

    fix(c): fix missing 'z' and 'Z' characters
    Fixed the missing 'z' and 'Z' characters from the
    printed output, by modifying the for loop logic.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints all lowercase alphabets, uppercase alphabets followed by 
+ * Description: Prints all lowercase alphabets, uppercase alphabets followed by
  * a new line.
  *
  * Return: Always 0 (Success)
@@ -12,11 +12,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 'a'; i != 'Z'; i++)
+	for (i = 'a'; i != 'Z' + 1; i++)
 	{
+		putchar(i);
 		if (i == 'z')
 			i = 'A';
-		putchar(i);
 	}
 
 	putchar('\n');
